b69eb8d57668cf2063686b3a02e72a47e71de709
Fede <84208153+Federicoluna1@users.noreply.github.com>
2021-07-04 20:12:23 -0300

    Last commit

---
+++ src/components/contador.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import '../App'
+
+function Contador() {
+    const contador = useSelector ((state) => state.numero);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <h1>Contador</h1>
+            <h3>{contador}</h3>
+            <div>
+                <button onClick={() => dispatch ({type: 'INCREMENTAR'})}>+</button>
+                <button onClick={() => dispatch ({type: 'DECREMENTAR'})}>-</button>
+                <button onClick={() => dispatch ({type: 'RESET'})}>'Volver a 0'</button>
+            </div>
+        </div>
+   );
+}
+
+   export default Contador
